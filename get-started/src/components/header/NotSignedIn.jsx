e279530d6ef7c9aa463278f9ec47a0ab79a3dbd9
Alex Nguyen <amn7417@mavs.uta.edu>
2024-07-02 13:06:12 -0500

    <style>: header contents change depending on logged in state

---
+++ get-started/src/components/header/NotSignedIn.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom'
+
+export default function () {
+  return (
+    <div className="flex-col z-20 flex-wrap gap-6 p-8 rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 justify-end w-full invisible opacity-0 translate-y-1 absolute top-full left-0 transition-all duration-300 scale-95 origin-top lg:relative lg:scale-100 lg:peer-checked:translate-y-0 lg:translate-y-0 lg:flex lg:flex-row lg:items-center lg:gap-0 lg:p-0 lg:bg-transparent lg:w-7/12 lg:visible lg:opacity-100 lg:border-none peer-checked:scale-100 peer-checked:opacity-100 peer-checked:visible lg:shadow-none dark:shadow-none dark:bg-gray-800 dark:border-gray-700">
+      <div className="text-gray-600 dark:text-gray-300 lg:pr-4 lg:w-auto w-full lg:pt-0">
+        <ul className="tracking-wide font-medium lg:text-sm flex-col flex lg:flex-row gap-6 lg:gap-0">
+          <li>
+            <Link
+              to="/home"
+              className="block md:px-4 transition hover:text-primary"
+            >
+              <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/login"
+              className="block md:px-4 transition hover:text-primary"
+            >
+              <span>Log-in</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/signup"
+              className="block md:px-4 transition hover:text-primary"
+            >
+              <span>Sign-up</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="mt-12 lg:mt-0">
+        <Link
+          to="/signup"
+          className="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+        >
+          <span className="relative text-sm font-semibold text-white">
+            Get Started
+          </span>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- get-started/src/components/header/index.jsx
+++ get-started/src/components/header/index.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from '../../contexts/authContext'
 import { doSignOut } from '../../firebase/auth'
 import Logo from './Logo'
+import NotSignedIn from './NotSignedIn'
 
 export default function Header() {
   const navigate = useNavigate()
@@ -12,43 +13,29 @@ export default function Header() {
       <header>
         <nav className="z-10 w-full absolute">
           <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6 flex items-center justify-between">
-            <Logo /> {/* Place the Logo component here */}
+            <Logo />
             <div
               aria-hidden="true"
               className="fixed z-10 inset-0 h-screen w-screen bg-white/70 backdrop-blur-2xl origin-bottom scale-y-0 transition duration-500 peer-checked:origin-top peer-checked:scale-y-100 lg:hidden dark:bg-gray-900/70"
             ></div>
-            <div className="flex-col z-20 flex-wrap gap-6 p-8 rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 justify-end w-full invisible opacity-0 translate-y-1 absolute top-full left-0 transition-all duration-300 scale-95 origin-top lg:relative lg:scale-100 lg:peer-checked:translate-y-0 lg:translate-y-0 lg:flex lg:flex-row lg:items-center lg:gap-0 lg:p-0 lg:bg-transparent lg:w-7/12 lg:visible lg:opacity-100 lg:border-none peer-checked:scale-100 peer-checked:opacity-100 peer-checked:visible lg:shadow-none dark:shadow-none dark:bg-gray-800 dark:border-gray-700">
-              <div className="text-gray-600 dark:text-gray-300 lg:pr-4 lg:w-auto w-full lg:pt-0">
-                <ul className="tracking-wide font-medium lg:text-sm flex-col flex lg:flex-row gap-6 lg:gap-0">
-                  <li>
-                    <Link
-                      to="/login"
-                      className="block md:px-4 transition hover:text-primary"
-                    >
-                      <span>Log-in</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/signup"
-                      className="block md:px-4 transition hover:text-primary"
-                    >
-                      <span>Sign-up</span>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="mt-12 lg:mt-0">
-                <Link
-                  to="/signup"
+            {userLoggedIn ? (
+              <>
+                <button
+                  onClick={() => {
+                    doSignOut().then(() => {
+                      navigate('/login')
+                    })
+                  }}
                   className="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                 >
                   <span className="relative text-sm font-semibold text-white">
-                    Get Started
+                    Log out
                   </span>
-                </Link>
-              </div>
-            </div>
+                </button>
+              </>
+            ) : (
+              <NotSignedIn />
+            )}
           </div>
         </nav>
       </header>
@@ -56,6 +43,9 @@ export default function Header() {
   )
 }
 
+/*
+
+                  */
 /*
 <div className="flex flex-col ">
         <nav className="flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200">
